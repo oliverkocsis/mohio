d302b7e52bcdba2501799a4424c5c3dce9b8f7d1
Oliver Kocsis <oliver.kocsis@gmail.com>
2020-09-08 23:17:45 +0200

    Use mohio as parameter in tree elements. Use mocked store. Running without firebase emulator. Fix unit tests.

--- src/components/MohioTreeElement.test.js
+++ src/components/MohioTreeElement.test.js
@@ -1,23 +1,35 @@
 import React from 'react';
-import { renderWithStore } from '../store/store.bdd';
-import { screen } from '@testing-library/react';
-import MohioTreeElement from './MohioTreeElement';
-import { testId as MohioTreeElementFlatTestId } from './MohioTreeElementFlat';
-import { testId as MohioTreeElementMultiLevelTestId } from './MohioTreeElementMultiLevel';
+import { screen, } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { renderComponentWithStore } from './ComponentTestUtils';
 
-test('renders MohioTreeElementFlat when no child provided', () => {
-  const name = 'MohioTreeElementFlat';
-  renderWithStore(<MohioTreeElement name={name} />);
-  const element = screen.getByTestId(MohioTreeElementFlatTestId);
-  expect(element).toHaveTextContent(name);
+import MohioTreeElement from './MohioTreeElement';
+
+test('when mohio does not have children then MohioTreeElementFlat is rendered', () => {
+  const mohio = {
+    id: 'c79d0a13-91eb-4f30-bbf3-dad8dc228303',
+    name: 'MohioTreeElementFlat',
+  };
+  renderComponentWithStore(<MohioTreeElement mohio={mohio} />);
+  const element = screen.getByText(mohio.name);
+  expect(element).toBeInTheDocument();
 });
 
-test('renders MohioTreeElementMultiLevel when children provided', () => {
-  const name = 'MohioTreeElementMultiLevel';
-  const children = [
-    { name: 'Child' },
-  ];
-  renderWithStore(<MohioTreeElement name={name} children={children} />);
-  const element = screen.getByTestId(MohioTreeElementMultiLevelTestId);
-  expect(element).toHaveTextContent(name);
+test('when mohio has children then MohioTreeElementMultiLevel is rendered', async () => {
+  const mohio = {
+    id: 'a012e594-46b0-44ba-a4eb-76b6a0fd8680',
+    name: 'MohioTreeElementMultiLevel',
+    children: [
+      {
+        id: 'ab96e45b-aff4-4799-9d44-5ec3dd9bda2a',
+        name: 'Child One',
+      }
+    ]
+  };
+  renderComponentWithStore(<MohioTreeElement mohio={mohio} />);
+  const element = screen.getByText(mohio.name);
+  expect(element).toBeInTheDocument();
+  userEvent.click(element);
+  const child = await screen.findByText(mohio.children[0].name);
+  expect(child).toBeInTheDocument();
 });
