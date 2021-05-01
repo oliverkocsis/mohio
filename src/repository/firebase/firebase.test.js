82ab9f9317af7d101c969027a688e158ce06c2be
Oliver Kocsis <oliver.kocsis@gmail.com>
2021-05-01 21:57:39 +0200

    Cleansing Fire

--- src/repository/firebase/firebase.test.js
+++
@@ -1,5 +0,0 @@
-import app from './firebase';
-
-test("app is initialized by default", () => {
-  expect(app).toBeDefined();
-});
--- src/store/actionTypes.js
+++
@@ -1,4 +0,0 @@
-export const CLEAR_STORE = 'CLEAR_STORE';
-export const SET_MOHIO_LIST = 'SET_MOHIO_LIST';
-export const SET_MOHIO_TREE = 'SET_MOHIO_TREE';
-export const SET_MOHIO_VIEW = 'SET_MOHIO_VIEW';
